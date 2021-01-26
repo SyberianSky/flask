78319dc1e613a98eb8343d9790eabd21974a1700
SyberianSky <89light@mail.ru>
2021-01-26 11:15:14 +0300

    final

--- telegram-bot1.py
+++ telegram-bot1.py
@@ -16,9 +16,41 @@ def repeat_all_messages(message):
     for name in names:
         if name in message.text.lower():
                     bot.send_message(message.chat.id, text=get_random_quote()) 
-           
+#@bot.message_handler(content_types=["text"])
+#def repeat_all_messages2(message):
+    names1 = ['Гвинт', 'гвинт', 'карты', 'картишки', 'Карты', 'Картишки', 'ХВЭНДЭ', 'хвэндэ', 'Хвэндэ', 'Гвинтец', 'гвинтец', 'Партия', 'Партию', 'партия', 'партию', 'партейку', 'Партейку', 'Партейка', 'партейка']
+    for name in names1:
+        if name in message.text.lower():
+                    bot.send_message(message.chat.id, 'Кто то тут сказал ГВИНТ?')
+#    names2 = ['два', 'Два', '2']
+#    for name in names2:
+#        if name in message.text.lower():
+#                    bot.send_message(message.chat.id, 'Хера')
+    names3 = ['Холера', 'холера']
+    for name in names3:
+        if name in message.text.lower():
+                    bot.send_message(message.chat.id, 'Зараза')
+    names4 = ['Зараза', 'зараза']
+    for name in names4:
+        if name in message.text.lower():
+                    bot.send_message(message.chat.id, 'Холера')
+#    names5 = ['Да', 'да']
+#    for name in names5:
+#        if name in message.text.lower():
+#                    bot.send_message(message.chat.id, 'Пизда')
 
-#ef main():
+    if message.text.lower() == 'два':
+        bot.send_message(message.chat.id, 'хера')
+    if message.text.lower() == 'Два':
+        bot.send_message(message.chat.id, 'хера')
+    if message.text.lower() == '2':
+        bot.send_message(message.chat.id, 'хера') 
+    if message.text.lower() == 'да':
+        bot.send_message(message.chat.id, 'Пизда')
+    if message.text.lower() == 'Да':
+        bot.send_message(message.chat.id, 'Пизда')
+        
+        #ef main():
 #       updater = Updater(token);
 #       dp = updater.dispatcher
 
